21097b972c2a305cc0298d210aa70d56f27b4a0a
Shashank Basavapatna Subramanya <shanki@mail.uni-paderborn.de>
2017-12-16 20:46:00 +0100

    Fixed the nullpointer exception bug with initialSeeds

--- src/main/java/ideal/InternalAnalysisProblem.java
+++ src/main/java/ideal/InternalAnalysisProblem.java
@@ -1,5 +1,7 @@
 package ideal;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,105 +26,111 @@ import soot.SootMethod;
 import soot.Unit;
 
 public class InternalAnalysisProblem<V> implements
-    IDETabulationProblem<UpdatableWrapper<Unit>, AccessGraph, UpdatableWrapper<SootMethod>, V, InterproceduralCFG<UpdatableWrapper<Unit>, UpdatableWrapper<SootMethod>>> {
+IDETabulationProblem<UpdatableWrapper<Unit>, AccessGraph, UpdatableWrapper<SootMethod>, V, InterproceduralCFG<UpdatableWrapper<Unit>, UpdatableWrapper<SootMethod>>> {
 
-  private InterproceduralCFG<UpdatableWrapper<Unit>, UpdatableWrapper<SootMethod>> icfg;
-  private PerSeedAnalysisContext<V> context;
-  private AnalysisEdgeFunctions<V> edgeFunctions;
-  private IPropagationController<UpdatableWrapper<Unit>, AccessGraph> propagationController;
-  private NonIdentityEdgeFlowHandler<V> nonIdentityEdgeFlowHandler;
-  private StandardFlowFunctions<V> flowFunctions;
-  public final static AccessGraph ZERO = new AccessGraph(null){
-	  public String toString(){
-		  return "{ZERO}";
-	  }
-  };
+	private InterproceduralCFG<UpdatableWrapper<Unit>, UpdatableWrapper<SootMethod>> icfg;
+	private PerSeedAnalysisContext<V> context;
+	private AnalysisEdgeFunctions<V> edgeFunctions;
+	private IPropagationController<UpdatableWrapper<Unit>, AccessGraph> propagationController;
+	private NonIdentityEdgeFlowHandler<V> nonIdentityEdgeFlowHandler;
+	private StandardFlowFunctions<V> flowFunctions;
+	public final static AccessGraph ZERO = new AccessGraph(null){
+		public String toString(){
+			return "{ZERO}";
+		}
+	};
 
-  InternalAnalysisProblem(IDEALAnalysisDefinition<V> analysisDefinition, PerSeedAnalysisContext<V> context) {
-    this.icfg = analysisDefinition.eIcfg();
-    this.edgeFunctions = analysisDefinition.edgeFunctions();
-    this.propagationController = analysisDefinition.propagationController();
-    this.nonIdentityEdgeFlowHandler = analysisDefinition.nonIdentityEdgeFlowHandler();
-    this.flowFunctions = analysisDefinition.flowFunctions(context);
-    this.context = context;
-  }
+	InternalAnalysisProblem(IDEALAnalysisDefinition<V> analysisDefinition, PerSeedAnalysisContext<V> context) {
+		this.icfg = analysisDefinition.eIcfg();
+		this.edgeFunctions = analysisDefinition.edgeFunctions();
+		this.propagationController = analysisDefinition.propagationController();
+		this.nonIdentityEdgeFlowHandler = analysisDefinition.nonIdentityEdgeFlowHandler();
+		this.flowFunctions = analysisDefinition.flowFunctions(context);
+		this.context = context;
+	}
 
-  @Override
-  public boolean followReturnsPastSeeds() {
-    return true;
-  }
+	@Override
+	public boolean followReturnsPastSeeds() {
+		return true;
+	}
 
-  @Override
-  public boolean autoAddZero() {
-    return false;
-  }
+	@Override
+	public boolean autoAddZero() {
+		return false;
+	}
 
-  @Override
-  public int numThreads() {
-    return 1;
-  }
+	@Override
+	public int numThreads() {
+		return 1;
+	}
 
-  @Override
-  public boolean computeValues() {
-    return false;
-  }
+	@Override
+	public boolean computeValues() {
+		return false;
+	}
 
-  @Override
-  public FlowFunctions<UpdatableWrapper<Unit>, AccessGraph, UpdatableWrapper<SootMethod>> flowFunctions() {
-    return flowFunctions;
-  }
+	@Override
+	public FlowFunctions<UpdatableWrapper<Unit>, AccessGraph, UpdatableWrapper<SootMethod>> flowFunctions() {
+		return flowFunctions;
+	}
 
-  @Override
-  public InterproceduralCFG<UpdatableWrapper<Unit>, UpdatableWrapper<SootMethod>> interproceduralCFG() {
-    return icfg;
-  }
+	@Override
+	public InterproceduralCFG<UpdatableWrapper<Unit>, UpdatableWrapper<SootMethod>> interproceduralCFG() {
+		return icfg;
+	}
 
-  @Override
-  public Map<UpdatableWrapper<Unit>, Set<AccessGraph>> initialSeeds() {
-    return null;
-  }
+	@Override
+	public Map<UpdatableWrapper<Unit>, Set<AccessGraph>> initialSeeds() {
+		IFactAtStatement seed = context.getSeed();
+		UpdatableWrapper<Unit> stmt = seed.getStmt();
+		Set<AccessGraph> factSet = new HashSet<>();
+		factSet.add(seed.getFact());
+		Map<UpdatableWrapper<Unit>, Set<AccessGraph>> initialSeed = new HashMap<>();
+		initialSeed.put(stmt, factSet);
+		return initialSeed;
+	}
 
-  @Override
-  public AccessGraph zeroValue() {
-    return ZERO;
-  }
+	@Override
+	public AccessGraph zeroValue() {
+		return ZERO;
+	}
 
-  @Override
-  public EdgeFunctions<UpdatableWrapper<Unit>, AccessGraph, UpdatableWrapper<SootMethod>, V> edgeFunctions() {
-    return new ForwardEdgeFunctions<>(context, edgeFunctions);
-  }
+	@Override
+	public EdgeFunctions<UpdatableWrapper<Unit>, AccessGraph, UpdatableWrapper<SootMethod>, V> edgeFunctions() {
+		return new ForwardEdgeFunctions<>(context, edgeFunctions);
+	}
 
-  @Override
-  public JoinLattice<V> joinLattice() {
-    return new JoinLattice<V>() {
+	@Override
+	public JoinLattice<V> joinLattice() {
+		return new JoinLattice<V>() {
 
-      @Override
-      public V topElement() {
-        return edgeFunctions.top();
-      }
+			@Override
+			public V topElement() {
+				return edgeFunctions.top();
+			}
 
-      @Override
-      public V bottomElement() {
-        return edgeFunctions.bottom();
-      }
+			@Override
+			public V bottomElement() {
+				return edgeFunctions.bottom();
+			}
 
-      @Override
-      public V join(V left, V right) {
-        if (left == topElement() && right == topElement()) {
-          return topElement();
-        }
-        if (left == bottomElement() && right == bottomElement()) {
-          return bottomElement();
-        }
-        return edgeFunctions.join(left, right);
-      }
-    };
-  }
+			@Override
+			public V join(V left, V right) {
+				if (left == topElement() && right == topElement()) {
+					return topElement();
+				}
+				if (left == bottomElement() && right == bottomElement()) {
+					return bottomElement();
+				}
+				return edgeFunctions.join(left, right);
+			}
+		};
+	}
 
-  @Override
-  public EdgeFunction<V> allTopFunction() {
-    return new AllTop<V>(edgeFunctions.top());
-  }
+	@Override
+	public EdgeFunction<V> allTopFunction() {
+		return new AllTop<V>(edgeFunctions.top());
+	}
 
 	@Override
 	public boolean recordEdges() {
